f5c563f6ab0618a5dc48fc8abc1cd9d79adf53be
David Davidson <daviddav@textio.com>
2020-10-17 16:26:37 -0700

    Add dynamic highlights

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { highlights } from "./highlights.json";
+import { dynamicHighlights } from "./dynamic-highlights.json";
 import { text } from "./strings";
 
 export {
     highlights,
     text,
+    dynamicHighlights,
 }
